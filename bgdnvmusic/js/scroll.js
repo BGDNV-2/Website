cfc7330acd6cda301e80b4c22cdd4fb00d22c942
BGDNV Music <bgdnvmusic@example.com>
2026-08-19 11:32:21 +0200

    Add contact page, add tiktok and soundcloud links, add transparency on header, add BeatCity Radio as an option on footer, remove submit, repair contact and submit music buttons

--- bgdnvmusic/js/scroll.js
+++ bgdnvmusic/js/scroll.js
@@ -1,43 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector("header");
 
-    const title = document.querySelector('section[id="1"] h1');
-
-    if (!title) return;
-
-    const finalText = "BGDNV";
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#$%&@!?";
-
-    const scrambleDuration = 3000;
-    const holdDuration = 5000;
-    const speed = 50;
-
-    function startScramble() {
-
-        const startTime = Date.now();
-
-        const interval = setInterval(() => {
-
-            let randomText = "";
-
-            for (let i = 0; i < finalText.length; i++) {
-                randomText += chars[
-                    Math.floor(Math.random() * chars.length)
-                ];
-            }
-
-            title.textContent = randomText;
-
-            if (Date.now() - startTime >= scrambleDuration) {
-
-                clearInterval(interval);
-
-                title.textContent = finalText;
-
-                setTimeout(startScramble, holdDuration);
-            }
-
-        }, speed);
+    function updateHeader() {
+        if (window.scrollY > 0) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
     }
 
-    startScramble();
+    window.addEventListener("scroll", updateHeader);
+
+    updateHeader();
 });
